d5212e4cc74e9ec8bbde9e46bc483a3e73d2ed28
Leticia Gonçalves <leticiagoncalves.tech@gmail.com>
2023-03-31 23:11:39 -0300

    adding carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,46 @@
 import { styled } from '../styles'
 import { HomeContainer, Product } from './../styles/pages/home'
-
+import { useKeenSlider } from 'keen-slider/react'
 import Image from 'next/image'
 
 import shirt1 from '../assets/shirts/1.png'
 import shirt2 from '../assets/shirts/2.png'
 import shirt3 from '../assets/shirts/3.png'
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-  color: 'white',
-  span: {
-    fontWeight: 'bold',
-  },
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  },
-})
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    },
+  })
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className='keen-slider__slide'>
         <Image src={shirt1} alt='' width={520} height={480} />
         <footer>
           <strong>Shirt X</strong>
           <span>US$ 99.99</span>
         </footer>
       </Product>
-      <Product>
+      <Product className='keen-slider__slide'>
         <Image src={shirt2} alt='' width={520} height={480} />
+        <footer>
+          <strong>Shirt X</strong>
+          <span>US$ 99.99</span>
+        </footer>
+      </Product>
+      <Product className='keen-slider__slide'>
+        <Image src={shirt3} alt='' width={520} height={480} />
+        <footer>
+          <strong>Shirt X</strong>
+          <span>US$ 99.99</span>
+        </footer>
+      </Product>
+      <Product className='keen-slider__slide'>
+        <Image src={shirt3} alt='' width={520} height={480} />
         <footer>
           <strong>Shirt X</strong>
           <span>US$ 99.99</span>
