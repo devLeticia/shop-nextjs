8f0afe50d1a90f3e1539e72a2e569ad527712bd9
Leticia Gonçalves <leticiagoncalves.tech@gmail.com>
2023-04-01 23:20:54 -0300

    adding 2 hours to revalidate products fetch, formatting price currency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,12 @@
-import { styled } from '../styles'
 import { HomeContainer, Product } from './../styles/pages/home'
-import { useKeenSlider } from 'keen-slider/react'
 import Image from 'next/image'
 
-import shirt1 from '../assets/shirts/1.png'
-import shirt2 from '../assets/shirts/2.png'
-import shirt3 from '../assets/shirts/3.png'
-import shirt4 from '../assets/shirts/4.png'
-
+import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
+
 import { stripe } from '../lib/stripe'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Stripe from 'stripe'
-import { Products } from './../../node_modules/stripe/esm/resources/Products'
 
 interface HomeProps {
   products: {
@@ -40,7 +34,7 @@ export default function Home({ products }: HomeProps) {
             <Image src={product.imageUrl} alt='' width={520} height={480} />
             <footer>
               <strong>{product.name}</strong>
-              <span>US$ {product.price}</span>
+              <span>{product.price}</span>
             </footer>
           </Product>
         )
@@ -49,24 +43,35 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+// export const getServerSideProps: GetServerSideProps = async () => {
+// getStaticProps does not work on dev enviroment
+// this will run only when we build the app in production to generate static pages in cache
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
   })
-  console.log('Aqui--->', response.data)
+
   const products = response.data.map((product) => {
     const price = product.default_price as Stripe.Price
+
+    // we format the currency here, rather in the html for two reasons?
+    // 1. The price doesnt change according to rendering
+    // 2. The fomratter will execute only one time (when caching) rather than everytime when rendering the html
     return {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
       url: product.url,
-      price: price.unit_amount / 100,
+      price: new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(price.unit_amount / 100),
     }
   })
   return {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
