7198ec37a129f9b9c6dfeedee407f514e2cce604
Leticia Gonçalves <leticiagoncalves.tech@gmail.com>
2023-04-02 21:40:42 -0300

    product page styling

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,26 @@
 import { useRouter } from 'next/router'
+import {
+  ProductContainer,
+  ImageContainer,
+  ProductDetails,
+} from './../../styles/pages/product'
 
 export default function Product() {
-    const {query} = useRouter()
-    return (
-        <h1>Produtos {query.id}</h1>
-    )
+  const { query } = useRouter()
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+          error adipisci voluptas fugit quibusdam quae pariatur doloremque ex
+          voluptate. Voluptatum corrupti perspiciatis tenetur facere ipsam error
+          id explicabo. Culpa, quos.
+        </p>
+        <button>Buy now</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
