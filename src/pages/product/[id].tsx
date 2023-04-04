31dd4a7aa88bd730e55c1ba3d400e23734a1c74c
Leticia Gonçalves <leticiagoncalves.tech@gmail.com>
2023-04-03 22:38:07 -0300

    prefetching product page with static props and fallback:true

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,26 +1,81 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { useRouter } from 'next/router'
+import Stripe from 'stripe'
+import { stripe } from '../../lib/stripe'
+import Image from 'next/image'
 import {
   ProductContainer,
   ImageContainer,
   ProductDetails,
 } from './../../styles/pages/product'
 
-export default function Product() {
-  const { query } = useRouter()
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    url: string
+    price: string
+    description: string
+  }
+}
+
+export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter()
+  if (isFallback) {
+    return <p>Loading...</p>
+  }
+
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt={''} />
+      </ImageContainer>
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          error adipisci voluptas fugit quibusdam quae pariatur doloremque ex
-          voluptate. Voluptatum corrupti perspiciatis tenetur facere ipsam error
-          id explicabo. Culpa, quos.
-        </p>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
         <button>Buy now</button>
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+// tell next what parameters we want next to cache
+export const getStaticPaths: GetStaticPaths = (async) => {
+  return {
+    paths: [{ params: { id: 'prod_NdAPqWsshrZYVy' } }],
+    fallback: true,
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  //genererate a page by product
+  const productId = params.id
+
+  //retrive product from stripe api
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        url: product.url,
+        price: new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD',
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hour
+  }
 }
--- src/pages/product/teste.tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router'
-
-export default function Teste() {
-    const { query } = useRouter()
-    return (
-        <h1>Testando {JSON.stringify(query)}</h1>
-    )
-}
