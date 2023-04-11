5df5ca8bf566e77e18d5ae3af77ab5f490e3a20e
Leticia Gonçalves <leticiagoncalves.tech@gmail.com>
2023-04-10 22:53:32 -0300

    adding styles to  success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,16 @@
+import Link from 'next/link'
+import { ImageContainer, SuccessContainer } from '../styles/pages/success'
+
 export default function Success() {
-    return (
-        <h1>Success</h1>
-    )
+  return (
+    <SuccessContainer>
+      <h1>Thank you for your purchase!</h1>
+      <ImageContainer></ImageContainer>
+      <p>
+        Uhuu! <strong>Letícia,</strong> Your order has been processed. Your{' '}
+        <strong>Tshirt SomethinNow</strong> is on the way
+      </p>
+      <Link href='/'></Link>
+    </SuccessContainer>
+  )
 }
